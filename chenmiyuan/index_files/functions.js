6b3b9e0a19e9cf27b26e7cf24a1f22ddc54eae11
iamxiaojianzheng <1272209235@qq.com>
2024-11-12 12:35:17 +0000

    Deploying to master from  @ 69ae086058a6293ca41a3188116ef5112a21905e 🚀

--- chenmiyuan/index_files/functions.js
+++ chenmiyuan/index_files/functions.js
@@ -1,57 +1,5 @@
 /*
  * http://love.hackerzhou.me
  */
-
 // variables
-var $win = $(window);
-var clientWidth = $win.width();
-var clientHeight = $win.height();
-
-$(window).resize(function() {
-    var newWidth = $win.width();
-    var newHeight = $win.height();
-    if (newWidth != clientWidth && newHeight != clientHeight) {
-        location.replace(location);
-    }
-});
-
-(function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
-})(jQuery);
-
-function timeElapse(date){
-	var seconds = (+ new Date() - date.getTime()) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = Math.floor(seconds % 60);
-	seconds = (seconds + "").replace(/\.\d*/, "");
-	var result = "<span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
-	$("#clock").html(result);
-}
+var $win=$(window),clientWidth=$win.width(),clientHeight=$win.height();function timeElapse(t){var i=(+new Date-t.getTime())/1e3,n=Math.floor(i/86400);i%=86400;var a=Math.floor(i/3600);a<10&&(a="0"+a),i%=3600;var e=Math.floor(i/60);e<10&&(e="0"+e);var l='<span class="digit">'+n+'</span> 天 <span class="digit">'+a+'</span> 小时 <span class="digit">'+e+'</span> 分钟 <span class="digit">'+(i=((i=Math.floor(i%60))+"").replace(/\.\d*/,""))+"</span> 秒";$("#clock").html(l)}$(window).resize((function(){var t=$win.width(),i=$win.height();t!=clientWidth&&i!=clientHeight&&location.replace(location)})),function(t){t.fn.typewriter=function(){return this.each((function(){var i=t(this),n=i.html(),a=0;i.html("");var e=setInterval((function(){"<"==n.substr(a,1)?a=n.indexOf(">",a)+1:a++,i.html(n.substring(0,a)+(1&a?"_":"")),a>=n.length&&clearInterval(e)}),75)})),this}}(jQuery);
